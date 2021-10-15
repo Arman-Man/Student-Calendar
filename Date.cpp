039cd6b970e81be92cdfcfeecec2d17930786edb
Arman-Man <84875686+Arman-Man@users.noreply.github.com>
2021-10-15 11:27:02 -0700

    Date and Time class

---
+++ Date.cpp
@@ -0,0 +1,125 @@
+#include "Date.h"
+
+ostream& operator<<(ostream& output, const Date& obj) {
+    output << obj.month << '/' << obj.day << '/' << obj.year;
+    return output;
+}
+
+istream& operator>>(istream& input, Date& obj) {
+    input >> obj.month;
+    input.ignore();
+    input >> obj.day;
+    input.ignore();
+    input >> obj.year;
+
+    return input;
+}
+
+int Date::checkDay(int testDay) const
+{
+    static const int daysPerMonth[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+
+    if (testDay > 0 && testDay <= daysPerMonth[month])
+    {
+        return testDay;
+    }
+
+    //determine whether testDay is valid for a specific month
+    if (month == 2 && testDay == 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
+    {
+        return testDay;
+    }
+
+    cout << "Invalid Day " << testDay << " was set to 1" << endl;
+    return 1;  //keep data in valid state
+}
+
+//constructor validates month and calls utility function to validate day
+Date::Date(int mn, int dy, int yr)
+{
+    setDate(mn, dy, yr);
+}
+
+void Date::setDate(int mn, int dy, int yr)
+{
+    if (mn > 0 && mn <= 12)
+    {
+        month = mn;
+    }
+
+    else
+    {
+        month = 1;
+    }
+
+    year = yr;  //could also validate year
+
+    day = checkDay(dy);  //to validate the day
+}
+
+Date& Date::setMonth(int mn) {
+    month = mn;
+    return *this;
+}
+
+Date& Date::setDay(int dy) {
+    day = dy;
+    return *this;
+}
+
+Date& Date::setYear(int yr) {
+    year = yr;
+    return *this;
+}
+
+int Date::getMonth() const {
+    return month;
+}
+
+int Date::getDay() const {
+    return day;
+}
+
+int Date::getYear() const {
+    return year;
+}
+
+bool Date::operator>(const Date& right) const {
+    if (getYear() > right.year) {
+        return true;
+    }
+    else if (getYear() == right.year && getMonth() > right.month) {
+        return true;
+    }
+    else if (getYear() == right.year && getMonth() == right.month && getDay() > right.day) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool Date::operator==(const Date& right) const {
+    if (getYear() == right.year && getMonth() == right.month && getDay() == right.day) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool Date::operator<(const Date& right) const {
+    return !(*this > right);
+}
+
+bool Date::operator>=(const Date& right) const {
+    return (*this > right || *this == right);
+}
+
+bool Date::operator<=(const Date& right) const {
+    return (*this < right || *this == right);
+}
+
+bool Date::operator!=(const Date& right) const {
+    return !(*this == right);
+}
---
+++ Time.h
@@ -0,0 +1,35 @@
+#ifndef	TIME_H
+#define TIME_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Time
+{
+    friend ostream& operator<<(ostream&, const Time&);    //prints standard time
+    friend istream& operator>>(istream&, Time&);    //enter time in standard time, stores as universal time, form HH:MM AM (or PM)
+
+private:
+    int hour;       //0-23 (24 hour clock format)
+    int minute;     //0-59
+    Time standardToUniversal(string);  //utility function, convert standard time to universal time, enter "am" or "pm" as parameter
+
+public:
+    Time(int = 0, int = 0);
+
+    Time& setTime(int, int);
+    Time& setHour(int);
+    Time& setMinute(int);
+
+    int getHour() const;
+    int getMinute() const;
+
+                                        //must be in form universal time before using either print function
+    void printUniversal() const;        //print universal time (24 hour clock)
+    void printStandard()const;          //print standard time (AM/PM)
+
+    double operator-(const Time&) const;
+};
+
+#endif
